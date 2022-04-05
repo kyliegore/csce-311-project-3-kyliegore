b9139ed3e6bc8fb197c232b31b4f5d9a27928640
Kylie Gore <70169955+kyliegore@users.noreply.github.com>
2022-04-04 23:57:09 -0400

    Update text-client.cc

--- text-client.cc
+++ text-client.cc
@@ -1,84 +1,104 @@
-#include <text-server.h>
 #include <text-client.h>
-#include <named_semaphore.h>
-#include <shm_store.h>
+#include <text-server.h>
 
 #include <cstddef>
 #include <iostream>
 
 namespace logger {
 
-Producer::Producer(const char shm_name[], const char mutex_name[])
+Consumer::Consumer(const char* shm_name, const char* shm_log_signal_name)
     : shm_name_(shm_name),
-      shm_log_signal_(mutex_name) {
-  // get shared memory log signal named semaphore
-  //   created by and locked by consumer
-  shm_log_signal_.Open();
-  std::cout << "SERVER STARTED" <<std::endl;
-}
-
-
-int Producer::Produce(const std::string& msg) {
-  //OPENS THE SHARED MEMORY (3)
-  std::clog << "\tMEMORY OPEN" <<std::endl;
-  // open shared memory and get file descriptor
-  int shm_fd = ::shm_open(shm_name_.c_str(), O_RDWR, 0);
+      shm_log_signal_(shm_log_signal_name) {
+  // open shared memory and capture file descriptor
+  int shm_fd = ::shm_open(shm_name, O_CREAT | O_EXCL | O_RDWR, 0660);
   if (shm_fd < 0) {
     std::cerr << ::strerror(errno) << std::endl;
 
-    return errno;
+    ::exit(errno);
   }
 
+  // set size of shared memory with file descriptor
+  const size_t kBuffer_size = 4096 - sizeof(size_t);  // page - buffer_size size
+  const size_t kSHM_size = sizeof(SharedMemoryStore) + kBuffer_size;
+  if (::ftruncate(shm_fd, kSHM_size) < 0) {
+    std::cerr << ::strerror(errno) << std::endl;
+
+    ::exit(errno);
+  }
 
   // get copy of mapped mem
-  SharedMemoryStore* store = static_cast<SharedMemoryStore*>(
-    ::mmap(nullptr,
-           sizeof(SharedMemoryStore),
-           PROT_READ | PROT_WRITE,
-           MAP_SHARED,
-           shm_fd,
-           0));
-  if (store == MAP_FAILED) {
+  const int kProt = PROT_READ | PROT_WRITE;
+  store_ = static_cast<SharedMemoryStore*>(
+    ::mmap(nullptr, sizeof(SharedMemoryStore), kProt, MAP_SHARED, shm_fd, 0));
+
+  if (store_ == MAP_FAILED) {
     std::cerr << ::strerror(errno) << std::endl;
 
-    return errno;
+    ::exit(errno);
   }
 
-  std::clog << "\tOPENING" << store << std::endl;
-  std::ifstream in(msg);
-  if(in.is_open()) {
-    while (!in.eof())
-    {
-      std::string message = "";
-      //strncpy(store->buffer, getline(in, message).c_str(), store->buffer_size);
-    }
-    
-  in.close();
-  }
+  // init memory map
+  *store_ = {};
+  store_->buffer_size = kBuffer_size;  // set store's buffer size
+
+  // create signal mux (unlocked by producer(s))
+  shm_log_signal_.Create(0);
+  shm_log_signal_.Open();
+}
 
 
-  // copy string msg into shared memory via strncpy ('n' for bounded copy)
-  strncpy(store->buffer, msg.c_str(), store->buffer_size);
+Consumer::~Consumer() {
+  // return copy of mapped mem, capture any error/exit code
+  int exit_code = ::munmap(store_, sizeof(SharedMemoryStore));
 
-
-  // signal consumer
-  shm_log_signal_.Up();
-
-  // release copy of mapped mem
-  int result = ::munmap(store, sizeof(SharedMemoryStore));
-  if (result < 0) {
+  // alert for error in ::munmap
+  if (exit_code < 0)
     std::cerr << ::strerror(errno) << std::endl;
 
-    return errno;
-  }
+  // delete shared memory map
+  if (::shm_unlink(shm_name_) < 0)
+    std::cerr << ::strerror(errno) << std::endl;
 
-  return 0;
+  // delete named semaphore
+  shm_log_signal_.Destroy();
+
+  ::exit(errno);
 }
+
+
+void Consumer::Consume(const char log_name[]) {
+  // write any logs to file
+  std::string msg;
+  while (true) {
+    shm_log_signal_.Down();  // block until occupied signal
+    std::ofstream fout(log_name, std::ofstream::app);
+
+    fout << store_->buffer << '\n';  // write to file
+
+    fout.close();
+  }
 }
+
+
+}  // namespace logger
+// Deletes kLogger memory (calls destructor)
+void LoggerSigTermHandler(int sig);
+
+
+logger::Consumer* log_writer;
+
 
 int main(int argc, char* argv[]) {
-  // wrappers::NamedSemaphore Create(int client);
-  // wrappers::NamedSemaphore Create(int server);
+  assert(argc == 4
+    && "usage: consumer <shared_memory_name> <log_signal_mux_name> <log_file_name>");
+
+  // set SIGTERM signal handler to unlink shm
+  ::signal(SIGTERM, LoggerSigTermHandler);
+  ::signal(SIGINT, LoggerSigTermHandler);
+
+  // build Consumer and start writing to file
+  log_writer = new logger::Consumer(argv[1], argv[2]);
+  log_writer->Consume(argv[3]);
 
   assert(argc == 3 && "producer <shared_mem_name> <log_mutex_name>");
 
@@ -101,6 +121,7 @@ int main(int argc, char* argv[]) {
   }
   std::cout << "Sent: " << msg << std::endl;
   log_writer.Produce(std::string(msg));
+
 
   return 0;
 }
